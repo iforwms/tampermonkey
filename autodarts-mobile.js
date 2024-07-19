f5acde7951120417724a194781f92aa14840e44f
Ifor Waldo Williams <ifor@cors.tech>
2024-07-19 11:21:16 +0800

    Quick commit - 2024/07/19 11:21

--- autodarts-mobile.js
+++ autodarts-mobile.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autodarts Mobile
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Fix Autodarts board manager for mobile.
 // @author       iforwms
 // @match        http://192.168.1.2:3180/*
@@ -38,8 +38,38 @@
     });
   }
 
-  waitForEl(".chakra-stack").then((el) => {
-    console.log("Element is ready");
-    console.log(el.textContent);
+  waitForEl(".chakra-text").then(() => {
+    console.log('updating styling');
+    const root = document.getElementById("root");
+    const menuDiv = root.childNodes[1];
+    const mainDiv = root.childNodes[2];
+
+    menuDiv.firstChild.firstChild.style.display = "none";
+    menuDiv.style.overflowX = 'auto';
+    menuDiv.firstChild.style.padding = '4px';
+    const menuItems = menuDiv.firstChild.childNodes[3];
+    menuItems.childNodes.forEach(item => item.childNodes[0].style.display = 'none');
+
+    const buttonsDiv = mainDiv.firstChild.firstChild.firstChild;
+    buttonsDiv.style.gridTemplateColumns = 'repeat(6, 1fr)';
+    buttonsDiv.childNodes[0].style.gridColumn = 'span 6 / span 6';
+    buttonsDiv.childNodes[1].style.gridColumn = 'span 3 / span 3';
+    buttonsDiv.childNodes[2].style.gridColumn = 'span 3 / span 3';
+    buttonsDiv.childNodes[3].style.gridColumn = 'span 2 / span 2';
+    buttonsDiv.childNodes[4].style.gridColumn = 'span 2 / span 2';
+    buttonsDiv.childNodes[5].style.gridColumn = 'span 2 / span 2';
+
+    const statsDiv = mainDiv.firstChild.firstChild.childNodes[1];
+    statsDiv.style.display = 'grid';
+    statsDiv.style.gridTemplateColumns = 'repeat(3, 1fr)';
+    statsDiv.childNodes[0].style.display = 'none';
+    statsDiv.childNodes[3].style.display = 'none';
+    statsDiv.childNodes[2].style.gridColumn = 'span 2 / span 2';
+    statsDiv.childNodes[4].style.gridColumn = 'span 3 / span 3';
+
+    const statusDiv = statsDiv.childNodes[4];
+    statusDiv.firstChild.style.display = 'grid';
+    statusDiv.firstChild.style.gridTemplateColumns = 'repeat(4, 1fr)';
+    statusDiv.firstChild.firstChild.style.gridColumn = 'span 4 / span 4';
   });
 })();
